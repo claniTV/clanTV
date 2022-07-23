b593d382ac822226da535873a3304e174bb3eb2a
clanTV <108497073+clanTV@users.noreply.github.com>
2022-07-23 17:12:46 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "clan://localhost/DCIM/clanTV/clanTV");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
