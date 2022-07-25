4919d7534f9795756a1d56d3891e94a307f9653b
clanTV <108497073+clanTV@users.noreply.github.com>
2022-07-25 22:23:35 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -236,7 +236,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "*^_^*", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "(≡^ܫ^≡)", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
